10572ffcad94f1a9c657206d3345e4131bee62bd
Khushi Singh <khushisingh10.08.2005@gmail.com>
2025-12-27 21:25:14 +0530

    Working in backend to create database schema

---
+++ backend/controllers/userController.js
@@ -0,0 +1,81 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const User = require('../models/User');
+
+const registerUser = async (req, res) => {
+    const{name , email , password} = req.body;
+
+    try{
+        if(!name || !email || !password){
+            return res.status(400).json({message: `Fill the required fields`});
+        }
+
+        const userExists = await user.findOne ({email});
+        if(userExists){
+            return res.status(400).json({message: `The user already exists`});
+        }
+
+        const user = await User.create({
+            name,
+            email,
+            password,
+        });
+
+        if (user){
+            res.status(201).json({
+                _id : user.id,
+                name : user.name,
+                email : user.email,
+                role : user.role,
+                token : generateToken(user._id),
+            });
+        }
+        else{
+            res.status(400).json({message: `Invalid data`});
+        }
+    }
+    catch(error){
+        res.status(500).json({message : error.message});
+    }
+
+};
+
+const loginUser = async (req , res) => {
+    const {email , password } = req.body;
+
+    try {
+        const user = await User.findOne({email});
+
+        if (user && (await user.matchPassword(password))){
+            res.json({
+                _id : user.id,
+                name : user.name,
+                email : user.email,
+                role : user.role,
+                token : generateToken(user._id),
+            });
+        }
+        else{
+            res.status(401).json({message:'Wrong credentials'});
+        }
+    }
+    catch(error){
+        res.status(500).json({message: error.message});
+    }
+};
+
+const getMe = async (req, res) => {
+  res.status(200).json(req.user);
+};
+
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: '5Sd', 
+  });
+};
+
+module.exports = {
+  registerUser,
+  loginUser,
+  getMe,
+};
